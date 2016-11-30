06e20cb8f2759c920d2ef733b20aeefef75f0ec0
Joe Crosetto <joecrosetto987@gmail.com>
2016-11-30 11:04:17 -0600

    concatinated js files

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,15 @@
 var gulp = require('gulp'),
 	gutil = require('gulp-util'),
-	coffee = require('gulp-coffee'); //nodejs command to get handle to gulp methods and properties
+	coffee = require('gulp-coffee'),
+	concat = require('gulp-concat'); //nodejs command to get handle to gulp methods and properties
 
 var coffeeSources = ['components/coffee/tagline.coffee']
-
+var jsSources = [
+	'components/scripts/rclick.js',
+	'components/scripts/pixgrid.js',
+	'components/scripts/tagline.js',
+	'components/scripts/template.js'
+];
 // log is an arbitrary name
 gulp.task('coffee', function() {
 	gulp.src(coffeeSources)
@@ -11,3 +17,9 @@ gulp.task('coffee', function() {
 			.on('error', gutil.log))
 		.pipe(gulp.dest('components/scripts')) // pipe the now compiled coffee script to the scripts folder
 });
+
+gulp.task('js', function() {
+	gulp.src(jsSources)
+		.pipe(concat('script.js'))
+		.pipe(gulp.dest('builds/development/js'))
+})
