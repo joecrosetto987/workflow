2f56efb638db9e44cba1999bf9e10a7548871ede
Joe Crosetto <joecrosetto987@gmail.com>
2016-11-30 14:15:35 -0600

    watch html and json files

--- gulpfile.js
+++ gulpfile.js
@@ -14,6 +14,8 @@ var jsSources = [
 	'components/scripts/template.js'
 ];
 var sassSources = ['components/sass/style.scss'];
+
+var htmlSources = ['builds/development/*.html'];
 // log is an arbitrary name
 gulp.task('coffee', function() {
 	gulp.src(coffeeSources)
@@ -46,6 +48,8 @@ gulp.task('watch', function() {
 	gulp.watch(coffeeSources, ['coffee']);
 	gulp.watch(jsSources, ['js']);
 	gulp.watch('components/sass/*.scss', ['compass']);
+	gulp.watch(htmlSources, ['html']);
+	gulp.watch('builds/development/js/*.json', ['json']);
 });
 
 gulp.task('connect', function() {
@@ -55,5 +59,14 @@ gulp.task('connect', function() {
 	});
 });
 
-gulp.task('default', ['coffee', 'js', 'compass', 'connect', 'watch']);
+gulp.task('html', function() {
+	gulp.src(htmlSources)
+		.pipe(connect.reload())
+})
+
+gulp.task('json', function() {
+	gulp.src('builds/development/js/*.json')
+		.pipe(connect.reload())
+})
+gulp.task('default', ['json', 'html', 'coffee', 'js', 'compass', 'connect', 'watch']);
 
